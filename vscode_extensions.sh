c558db42e7eef9f80542a5ded461982943778856
Rik <riktimmers@hotmail.com>
2021-06-21 21:34:56 +0200

    added scripts for installed used extensions, and comment on how to get a new list.

---
+++ vscode_extensions.sh
@@ -0,0 +1,18 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension betwo.b2-catkin-tools
+code --install-extension Compulim.compulim-vscode-closetag
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension ms-iot.vscode-ros
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.test-adapter-converter
+code --install-extension mutantdino.resourcemonitor
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension twxs.cmake
+code --install-extension vscodevim.vim
